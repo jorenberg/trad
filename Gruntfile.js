e73d7dc83945c89526b1e2b49c0f9d033af6bdfb
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-11 01:38:06 +0530

    Initial commit
    The Heart of Build System of "Träd®".

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,24 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Träd® [v1.0.0]
+ * ~~~~~~~~~~~~~~
+ * ______________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________
+ * @date      : 11-March-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "Träd®".
+ * ______________________________________________________________
+ */
