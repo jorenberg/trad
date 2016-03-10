b4aeb7ce5c776b94bcb37021541656e35398d609
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-11 02:53:00 +0530

    Initial commit
    The Heart of NPM System for "Träd®".

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 #!/usr/bin/env node
+
+
+/*!
+ *  ====||====  ||======    //====\\  ||======||
+ *      ||      ||    ||    ||    ||  ||      ||
+ *      ||      ||    ||    ||    ||  ||      ||
+ *      ||      ||======    ||======  ||      ||
+ *      ||      ||    \\    ||    ||  ||      ||
+ *      ||      ||      \\  ||    ||  ||======||
+ */
+/*!
+ * Träd® [v1.0.0]
+ * ~~~~~~~~~~~~~~
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________
+ * @date      : 11-March-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of NPM System/-- of "Träd®".
+ * ______________________________________________________________
+ */
